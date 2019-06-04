9e8256383f423af0b543314bdb1ca7cb85934eae
Info Aakvatech <info@aakvatech.com>
2019-06-04 13:40:48 +0000

    fix: Difference HEADers were added to init.py

--- propms/__init__.py
+++ propms/__init__.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-<<<<<<< HEAD
-__version__ = '1.2.3'
-=======
-__version__ = '1.2.2'
->>>>>>> 3dc3245d5d501638b76e2578d85eaf6eef4c21e2
+__version__ = '1.2.4'
 
