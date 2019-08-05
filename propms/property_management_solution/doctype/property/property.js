6afa6cc67f8147eb43b389f060db0eb8c19078e8
Info Aakvatech <info@aakvatech.com>
2019-08-05 08:42:47 +0000

    Fix: Updates for managing meter reading module

--- propms/property_management_solution/doctype/property/property.js
+++ propms/property_management_solution/doctype/property/property.js
@@ -14,7 +14,20 @@ frappe.ui.form.on('Property Meter Reading', {
 		if (meter_doc.meter_number != "") {
 			$.each(property_doc.property_meter_reading, function(i, d) {
 				if(d.name!=meter_doc.name && meter_doc.meter_type==d.meter_type && d.status=="Active")	{
-					var msg="Another Active Meter of type "+meter_doc.meter_type+" Is Already allocated. Please de-activate it before adding a new meter of same type."
+					var msg=_("Another Active Meter of type ")+meter_doc.meter_type+_(" Is Already allocated. Please de-activate it before adding a new meter of same type.")
+					frappe.model.set_value(cdt,cdn,"meter_number",'')
+					frappe.throw(msg)
+				}
+			})
+		}
+	},
+	status: function(frm,cdt,cdn) {
+		var property_doc = locals[cur_frm.doc.doctype][cur_frm.doc.name];
+		var meter_doc = locals[cdt][cdn];
+		if (meter_doc.meter_number != "") {
+			$.each(property_doc.property_meter_reading, function(i, d) {
+				if(d.name!=meter_doc.name && meter_doc.meter_type==d.meter_type && d.status=="Active")	{
+					var msg=_("Another Active Meter of type ")+meter_doc.meter_type+_(" Is Already allocated. Please de-activate it before adding a new meter of same type.")
 					frappe.model.set_value(cdt,cdn,"meter_number",'')
 					frappe.throw(msg)
 				}
