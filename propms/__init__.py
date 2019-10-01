d6866a542ea0657b84fc03dd0f487fa1b95a9fb5
Info Aakvatech <info@aakvatech.com>
2019-10-01 08:24:08 +0000

    fix: Security Deposit Added menu entry in PropMS Section

--- propms/__init__.py
+++ propms/__init__.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-<<<<<<< HEAD
-__version__ = '1.4.12'
-=======
-__version__ = '1.4.13'
->>>>>>> f7f8c7626c31a2bc274164287a9c90c248f99d78
+__version__ = '1.4.14'
+
 
