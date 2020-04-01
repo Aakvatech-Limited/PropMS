28f1fbe0cc374e6ba774d98eeca7022d2b6810c9
Aakvatech Limited <info@aakvatech.com>
2020-04-01 06:53:53 +0000

    Update auto_custom.py and removed get_exchange_rate

--- propms/auto_custom.py
+++ propms/auto_custom.py
@@ -335,63 +335,6 @@ def getDateMonthDiff(start_date, end_date, month_factor):
 	return month_count
 
 
-def get_exchange_rate(from_currency, to_currency, transaction_date=None, args=None):
-	if not (from_currency and to_currency):
-		# manqala 19/09/2016: Should this be an empty return or should it throw and exception?
-		print("No need to convert")
-		return
-	if from_currency == to_currency:
-		return 1
-	if not transaction_date:
-		transaction_date = today()
-	currency_settings = frappe.get_doc("Accounts Settings").as_dict()
-	print(transaction_date)
-	allow_stale_rates = currency_settings.get("allow_stale")
-	filters = [
-		["date", "<=", get_datetime_str(transaction_date)],
-		["from_currency", "=", from_currency],
-		["to_currency", "=", to_currency]
-	]
-	if args == "for_buying":
-		filters.append(["for_buying", "=", "1"])
-	elif args == "for_selling":
-		filters.append(["for_selling", "=", "1"])
-	if not allow_stale_rates:
-		stale_days = currency_settings.get("stale_days")
-		checkpoint_date = add_days(transaction_date, -stale_days)
-		filters.append(["date", ">", get_datetime_str(checkpoint_date)])
-
-	# cksgb 19/09/2016: get last entry in Currency Exchange with from_currency and to_currency.
-	entries = frappe.get_all(
-		"Currency Exchange", fields=["exchange_rate"], filters=filters, order_by="date desc",
-		limit=1)
-	if entries:
-		return flt(entries[0].exchange_rate)
-
-	try:
-		cache = frappe.cache()
-		key = "currency_exchange_rate:{0}:{1}".format(from_currency, to_currency)
-		value = cache.get(key)
-
-		if not value:
-			import requests
-			print('Trying to get from Frankfurter')
-			api_url = "https://frankfurter.erpnext.org/{0}".format(transaction_date)
-			response = requests.get(api_url, params={
-				"base": from_currency,
-				"symbols": to_currency
-			})
-			# expire in 6 hours
-			response.raise_for_status()
-			value = response.json()["rates"][to_currency]
-			print(value)
-			cache.setex(key, value, 6 * 60 * 60)
-		return flt(value)
-	except:
-		frappe.msgprint(_("Unable to find exchange rate for {0} to {1} for key date {2}. Please create a Currency Exchange record manually").format(from_currency, to_currency, transaction_date))
-		return 0.0
-
-
 @frappe.whitelist()
 def get_active_meter_from_property(property_id,meter_type):
 	"""Get Active Meter Number"""
