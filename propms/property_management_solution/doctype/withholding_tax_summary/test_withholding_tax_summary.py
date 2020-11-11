b711ba3e5e8ec12a39dd0b3645a1bbee09551218
Info Aakvatech <info@aakvatech.com>
2020-11-11 04:45:56 +0000

    chore: cleared wtax summary doctype that wasn't necessary anymore.

--- propms/property_management_solution/doctype/withholding_tax_summary/test_withholding_tax_summary.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2018, Aakvatech and Contributors
-# See license.txt
-from __future__ import unicode_literals
-import unittest
-
-class TestWithholdingTaxSummary(unittest.TestCase):
-	pass
--- propms/property_management_solution/doctype/withholding_tax_summary/withholding_tax_summary.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2018, Aakvatech and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Withholding Tax Summary', {
-	refresh: function(frm) {
-
-	}
-});
