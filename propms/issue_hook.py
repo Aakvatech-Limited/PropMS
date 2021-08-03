6952a8e59723ab88c78855e6c9018ebe1326ec1a
aakvatech <35020381+aakvatech@users.noreply.github.com>
2021-08-03 15:57:31 +0300

    chore: Removed unused get_mode_of_payment

--- propms/issue_hook.py
+++ propms/issue_hook.py
@@ -4,7 +4,6 @@ from frappe import _
 from frappe.utils import today
 from erpnext.utilities.product import get_price
 from erpnext.stock.get_item_details import get_pos_profile
-from erpnext.accounts.doctype.sales_invoice.pos import get_mode_of_payment
 from propms.auto_custom import get_latest_active_lease
 from erpnext.controllers.accounts_controller import get_taxes_and_charges
 
