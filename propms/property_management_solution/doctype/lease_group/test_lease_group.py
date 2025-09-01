ed4e9acdede724a3db91fe9f4efaef4309c9061b
iphenelist <innocentphenelist@gmail.com>
2025-09-01 12:13:19 +0300

    feat: add Lease Group functionality with validation and cancellation logic

---
+++ propms/property_management_solution/doctype/lease_group/test_lease_group.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Aakvatech and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLeaseGroup(FrappeTestCase):
+	pass
